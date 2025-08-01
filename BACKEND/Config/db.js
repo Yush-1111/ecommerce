d249e0e90d82c1e7fe7e3a8dd4202ef703ce174b
yush-1111 <yush@1111>
2025-08-01 20:51:22 +0530

    removed frontend build directory

--- BACKEND/Config/db.js
+++ BACKEND/Config/db.js
@@ -3,9 +3,6 @@ const mongoose = require("mongoose")
 async function connectDB(){
     try{
        await mongoose.connect(process.env.MONGODB_URI)
-    
-        
-
     }catch (err) {
         
         throw new Error(err);
